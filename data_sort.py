e79141c57ea909c7acecbef9a7c53e924156226e
Sheharyar Ali <sheharyarali04052000@gmail.com>
2022-12-01 11:24:59 +0100

    added files

---
+++ data_sort.py
@@ -0,0 +1,36 @@
+file = open("Operations_data.csv", "r")
+Transfer_time = []
+Arr = []
+Dep = []
+Span = []
+ArrInt = []
+DepInt = []
+Pax = []
+for lines in file.readlines():
+    lines = lines[:-1]
+    columns = lines.split(",")
+    Transfer_time.append(columns[0].strip())
+    Arr.append(columns[1].strip())
+    Dep.append(columns[2].strip())
+    Span.append(columns[3].strip())
+    ArrInt.append(columns[4].strip())
+    DepInt.append(columns[5].strip())
+    Pax.append(int(columns[6].strip()))
+
+# Fixing ArrInt and DepInt
+for i in range(0, len(ArrInt)):
+    if ArrInt[i] == "":
+        ArrInt[i] = 0
+    elif ArrInt[i] == "1":
+        ArrInt[i] = 1
+for i in range(0, len(DepInt)):
+    if DepInt[i] == "":
+        DepInt[i] = 0
+    elif DepInt[i] == "1":
+        DepInt[i] = 1
+
+# Calculating time spent on ground in minutes
+for i in range(0, len(Transfer_time)):
+    Value = Transfer_time[i]
+    Time = (int(Value[0] + Value[1]) * 60) + (int(Value[-2] + Value[-1]))
+    Transfer_time[i] = Time
